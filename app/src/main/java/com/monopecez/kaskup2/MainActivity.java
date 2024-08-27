00433398eb584024905ade0580e892c61091bb6b
Isman Junian <isman.rahman@tokopedia.com>
2024-08-27 20:55:43 +0700

    add calculate price method
    Change-Id: Ifd53d057659293e0f3b6e850ad4a4baa55a54958

--- app/src/main/java/com/monopecez/kaskup2/MainActivity.java
+++ app/src/main/java/com/monopecez/kaskup2/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,31 +15,54 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    static int[] qtyList, harga;
+    static int menuSize;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String[] name = new String[] {"Kupat", "Kari"};
-        int[] harga = new int[]{10000, 20000};
+        String[] name = new String[] {"Kupat", "Kari", "Tahu"};
+        harga = new int[]{10000, 20000, 200};
+
+        menuSize = harga.length;
 
         setContentView(R.layout.activity_main);
 
         ConstraintLayout mainConstraintLayout = findViewById(R.id.mainConstraintLayout);
 
-        ConstraintLayout block1 = buildBlock(R.id.ph16, "ASFFF", 200, true);
+        ConstraintLayout temp = new ConstraintLayout(this);
 
-        mainConstraintLayout.addView(block1);
+        qtyList = new int[menuSize];
+        for(int i = 0; i < menuSize; i++){
+            if (i == 0){
+                temp = buildBlock(R.id.kupatset, name[i], harga[i], true, i);
+            } else {
+                temp = buildBlock(temp.getId(), name[i], harga[i], true, i);
+            }
+            mainConstraintLayout.addView(temp);
+            qtyList[i] = 0;
+
+        }
 
     }
 
-    private ConstraintLayout buildBlock(int beforeComponent, String menuNameInput, int harga, boolean isEven){
+    private void calculateTotal(){
+        TextView totalText = (TextView) findViewById(R.id.totalharga);
+        int total = 0;
+        for (int i = 0; i < menuSize; i++){
+            total = total + (qtyList[i] * harga[i]);
+        }
+        totalText.setText(String.valueOf(total));
+    }
+
+    private ConstraintLayout buildBlock(int beforeComponent, String menuNameInput, int harga, boolean isEven, int idx){
         float dpCoeficient = this.getResources().getDisplayMetrics().density;
 
-        String background = "";
+        int background = 0;
         if (isEven) {
-            background = "#e8e8e8";
+            background = 15263976; // "#e8e8e8";
         } else {
-            background = "#ffffff";
+            background = 0; // "#ffffff";
         }
         ConstraintLayout outLayout = new ConstraintLayout(this);
         int layoutId = View.generateViewId();
@@ -45,17 +70,22 @@ public class MainActivity extends AppCompatActivity {
         ConstraintLayout.LayoutParams outLayoutParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         outLayoutParams.topToBottom = beforeComponent;
         outLayout.setLayoutParams(outLayoutParams);
+        outLayout.setBackgroundColor(background);
         outLayout.setId(layoutId);
 
 
         // TEXT VIEW
         TextView menuName = new TextView(this);
         menuName.setText(menuNameInput);
+        menuName.setTypeface(null, Typeface.BOLD);
         menuName.setId(View.generateViewId());
         menuName.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        menuName.setGravity(Gravity.CENTER);
 
-        ConstraintLayout.LayoutParams menuNameParams = new ConstraintLayout.LayoutParams((int) dpCoeficient * 128, 0);
+        ConstraintLayout.LayoutParams menuNameParams = new ConstraintLayout.LayoutParams((int) (dpCoeficient * 128 * 1.15 ), 0);
         menuNameParams.verticalBias = .857f;
+        menuNameParams.topToTop = outLayout.getId();
+        menuNameParams.bottomToBottom = outLayout.getId();
         menuNameParams.setMarginStart(16);
 
         // BUTTON KURANG
@@ -63,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         menuKurang.setText("-");
         menuKurang.setId(View.generateViewId());
 
-        ConstraintLayout.LayoutParams menuKurangParams = new ConstraintLayout.LayoutParams((int) dpCoeficient * 46, (int) dpCoeficient* 46);
+        ConstraintLayout.LayoutParams menuKurangParams = new ConstraintLayout.LayoutParams((int) (dpCoeficient * 46), (int) (dpCoeficient* 46));
         menuKurangParams.setMarginStart(8);
 
         // BUTTON TAMBAH
@@ -71,19 +101,26 @@ public class MainActivity extends AppCompatActivity {
         menuTambah.setText("+");
         menuTambah.setId(View.generateViewId());
 
-        ConstraintLayout.LayoutParams menuTambahParams = new ConstraintLayout.LayoutParams((int) dpCoeficient* 46, (int)  dpCoeficient* 46);
+        ConstraintLayout.LayoutParams menuTambahParams = new ConstraintLayout.LayoutParams((int) (dpCoeficient * 46), (int) (dpCoeficient* 46));
         menuTambahParams.setMarginStart(8);
 
 
         // TEXT VIEW TOTAL
         TextView menuTotal = new TextView(this);
         menuTotal.setText("0 | 0");
+        menuTotal.setTypeface(null, Typeface.BOLD);
         menuTotal.setId(View.generateViewId());
 
         ConstraintLayout.LayoutParams menuTotalParams = new ConstraintLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
         menuTotalParams.setMarginStart(8);
+        menuTotalParams.topToTop = outLayout.getId();
+        menuTotalParams.bottomToBottom = outLayout.getId();
 
-
+        menuNameParams.leftToLeft = outLayout.getId();
+        menuKurangParams.leftToRight = menuName.getId();
+        menuTambahParams.leftToRight = menuKurang.getId();
+        menuTotalParams.rightToRight = outLayout.getId();
+        menuTotalParams.setMarginEnd(20);
 
         menuName.setLayoutParams(menuNameParams);
         menuKurang.setLayoutParams(menuKurangParams);
@@ -95,18 +132,33 @@ public class MainActivity extends AppCompatActivity {
         outLayout.addView(menuTambah);
         outLayout.addView(menuTotal);
 
-
         ConstraintSet cs = new ConstraintSet();
         cs.clone(outLayout);
-        cs.connect(menuName.getId(), ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
-        cs.connect(menuKurang.getId(), ConstraintSet.START, menuName.getId(), ConstraintSet.END);
-        cs.connect(menuTambah.getId(), ConstraintSet.START, menuKurang.getId(), ConstraintSet.END);
-        cs.connect(menuTotal.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
-        cs.connect(menuTotal.getId(), ConstraintSet.START, menuTambah.getId(), ConstraintSet.END);
         cs.applyTo(outLayout);
 
+        menuTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int qty = qtyList[idx] + 1;
+                qtyList[idx] = qty;
+                menuTotal.setText(qty + " | " + (qty*harga));
+                calculateTotal();
+            }
+        });
+
+        menuKurang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int qty = qtyList[idx];
+                if (qty >= 1){
+                    qty--;
+                }
+                qtyList[idx] = qty;
+                menuTotal.setText(qty + " | " + (qty*harga));
+                calculateTotal();
+            }
+        });
 
         return outLayout;
-
     }
 }
